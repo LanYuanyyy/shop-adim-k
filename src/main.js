df719c6dbd57b61e1782f9f195c247c650f69710
project <you@pro.com>
2019-06-28 16:36:48 +0800

    登陆功能完成了

--- src/main.js
+++ src/main.js
@@ -13,6 +13,30 @@ Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
+
+// 注册一个导航守卫
+router.beforeEach((to, from, next) => {
+  // 判断用户是不是访问登陆页面
+  if(to.path === "/Login"){
+    next()
+    return
+  }
+  // 如果是访问其他页面 判断用户是否进行登陆了
+  // 如果没登陆 直接跳转到登陆页面
+  // 如果登陆了 那就直接放行
+
+  if(localStorage.getItem('token')){
+    next()
+  }else{
+    // 如果没有返回登陆页面
+    router.push('/Login')
+  }
+  // console.log('导航守卫触发了')
+  // next 提供页面跳转  调用方法会进行跳转
+  // next()
+  //
+})
+
 new Vue({
   router,
   render: h => h(App)
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -32,7 +32,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   data() {
@@ -49,10 +49,10 @@ export default {
             trigger: "blur"
           },
           {
-            min:5,
-            max:12,
-            message:'用户名必须5-12字符之间',
-            trigger:'change'
+            min: 5,
+            max: 12,
+            message: "用户名必须5-12字符之间",
+            trigger: "change"
           }
         ],
         password: [
@@ -62,10 +62,10 @@ export default {
             trigger: "blur"
           },
           {
-            min:6,
-            max:12,
-            message:'密码必须是6-12位',
-            trigger:'change'
+            min: 6,
+            max: 12,
+            message: "密码必须是6-12位",
+            trigger: "change"
           }
         ]
       }
@@ -76,15 +76,21 @@ export default {
       this.$refs[formName].validate(valid => {
         if (valid) {
           axios({
-            url:'http://localhost:8888/api/private/v1/Login',
-            method:'post',
+            url: "http://localhost:8888/api/private/v1/Login",
+            method: "post",
             data: this.form
-          }).then(res => {
+          }).then(({data:{data,meta}}) => {
             // console.log(res)
-            if(res.data.meta.status === 200) {
-              // console.log(res.data.meta.msg)
+            if (meta.status === 200) {
+            //   console.log(res.data);
               // 编程式导航跳转页面
-              this.$router.push("./home")
+
+              // 登陆成功之后，服务器端会返回一个token
+              // 我们需要将这个token保存到本地
+              // 保存到localstorage中
+              localStorage.setItem('token', data.token);
+
+              this.$router.push("/home");
             }
           })
         } else {
