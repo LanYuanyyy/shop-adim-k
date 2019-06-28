1283a39e0680eaa1fd2c9207c49cac13a9747c0c
project <you@pro.com>
2019-06-28 14:49:49 +0800

    表单校验和登陆重置按钮实现

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,18 +6,25 @@
     <!-- el-col 布局组件 列组件 -->
     <!-- span 属性是用来设置当前列所占的格数 -->
     <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
-      <el-form ref="form" :model="form" label-width="80px" label-position="top" class="form">
-        <el-form-item label="用户名">
+      <el-form
+        ref="Loginform"
+        :rules="formRules"
+        :model="form"
+        label-width="80px"
+        label-position="top"
+        class="form"
+      >
+        <el-form-item label="用户名" prop="username">
           <el-input v-model="form.username"></el-input>
         </el-form-item>
 
-        <el-form-item label="密码">
+        <el-form-item label="密码" prop="password">
           <el-input v-model="form.password"></el-input>
         </el-form-item>
 
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">登陆</el-button>
-          <el-button>重置</el-button>
+          <el-button type="primary" @click="submitForm('Loginform')">登陆</el-button>
+          <el-button @click="resetForm('Loginform')">重置</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -31,12 +38,49 @@ export default {
       form: {
         username: "",
         password: ""
+      },
+      formRules: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur"
+          },
+          {
+            min:5,
+            max:12,
+            message:'用户名必须5-12字符之间',
+            trigger:'change'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur"
+          },
+          {
+            min:6,
+            max:12,
+            message:'密码必须是6-12位',
+            trigger:'change'
+          }
+        ]
       }
     };
   },
   methods: {
-    onSubmit() {
-      console.log("submit!");
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     }
   }
 };
