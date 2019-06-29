683806ce22f2c5bf4216b5f6bdc2cd2e66584c9d
project <you@pro.com>
2019-06-29 20:55:31 +0800

    home的头部和菜单栏的样式完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,145 @@
 <template>
-  <h1>
-    我是首页 如果用户没登陆直接访问  需要判断
-  </h1>
+  <el-container class="box">
+    <el-header>
+      <el-row type="flex" justify="space-between" align="middle">
+        <el-col :span="6">
+          <img src="./../assets/logo.png" alt />
+        </el-col>
+        <el-col>
+          <h1>品优购后台管理系统</h1>
+        </el-col>
+        <el-col :span="6" class="header-right">
+          <div>
+            39期会员管理
+            <a href="#">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item>
+              <i class="el-icon-menu"></i>
+              <span>用户管理</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item>
+              <i class="el-icon-menu"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item>
+              <i class="el-icon-menu"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item>
+              <i class="el-icon-menu"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item>
+              <i class="el-icon-menu"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item>
+              <i class="el-icon-menu"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item>
+              <i class="el-icon-menu"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item>
+              <i class="el-icon-menu"></i>
+              <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+          
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
+
+<style>
+.el-container {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+.el-container .el-header {
+  background-color: skyblue;
+  padding: 0;
+  margin: 0;
+}
+
+.el-container .el-header h1 {
+  padding: 0;
+  margin: 0;
+  text-align: center;
+  line-height: 60px;
+  color: aliceblue;
+}
+
+.el-container .el-header img {
+  width: 200px;
+}
+
+.el-container .el-header div a {
+  color: orange;
+  /* display: none; */
+}
+
+.el-aside {
+  background-color: hotpink;
+}
+
+.header-right {
+  font-weight: bolder;
+  text-align: right;
+  padding-right: 10px;
+}
+
+.el-menu-vertical-demo.el-menu {
+  height: 100%;
+}
+</style>
